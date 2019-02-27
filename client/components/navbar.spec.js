466aea367891038ce3c2dc40caacdbd6a317eec9
April Daley <daley.april@gmail.com>
2019-02-26 20:32:25 -0500

    feat(navbar): tweaks to test specs

--- client/components/navbar.spec.js
+++ client/components/navbar.spec.js
@@ -7,19 +7,17 @@ import {Navbar} from './navbar'
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-console.log('Navbar 1: ', Navbar)
-
 describe('Navbar', () => {
   let navbar
 
   beforeEach(() => {
-    console.log('Navbar: ', Navbar)
     navbar = shallow(<Navbar />)
   })
 
-  //   it('links to the login page', () => {
-  //     expect(navbar.find('href').text()).to.be.equal('/login')
-  //   })
+  it('links to the login page', () => {
+    //     expect(navbar.find('href')).to.be.equal('/login')
+    expect(navbar.find(`a[href='/login']`)).to.be.present()
+  })
 
   it('links to the sign-up page', () => {
     expect(navbar.find('href')).to.be.equal('/signup')
