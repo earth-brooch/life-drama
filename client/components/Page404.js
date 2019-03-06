96b72ee7ea0073cb01e87d6b37436fe3ba536ddb
Sara Parelhoff <sara.parelhoff@gmail.com>
2019-03-05 23:57:50 -0500

    TWEAKS: Updated seed file, added font, 404 page, Keep Shopping button doesn't refresh

---
+++ client/components/Page404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page404 = () => (
+  <div className="not-found">
+    <div className="nf-image">
+      4-<img src="/surprise.png" />-4
+    </div>
+    WHOOPS! YOU DON'T BELONG HERE!
+    <a href="/">
+      <h1>GO HOME</h1>
+    </a>
+  </div>
+)
+
+export default Page404
--- script/seed.js
+++ script/seed.js
@@ -14,13 +14,48 @@ async function seed() {
   ])
 
   const product = await Promise.all([
-    Product.create({name: 'Dramatic Entrance', price: 50}),
-    Product.create({name: 'Evil Twin', price: 250}),
-    Product.create({name: 'Back from the Dead', price: 500}),
-    Product.create({name: 'Love Triangle', price: 80}),
-    Product.create({name: 'Evil Stepmother', price: 240}),
-    Product.create({name: 'Chased by a Mob', price: 75}),
-    Product.create({name: 'In Love with a Criminal', price: 1000})
+    Product.create({
+      name: 'Dramatic Entrance',
+      description:
+        "Crash your ex-lover's wedding via parachute, liven up a funeral by giving your eulogy after emerging from inside the coffin, pick up your high school diploma from the back of a gleaming white horse. If you want to pull focus and BE the one everyone is looking at, look no further!",
+      price: 50
+    }),
+    Product.create({
+      name: 'Evil Twin',
+      description:
+        "Like looking in a human mirror, except EVIL! This identical human being will make you look like an incredible human being by comparison. All kinds of shenanigans may ensue before your friends and family catch on that there's TWO of you now!",
+      price: 250
+    }),
+    Product.create({
+      name: 'Back from the Dead',
+      description:
+        'The next generation of our insanely popular "Now They\'ll Be Sorry," package, "Back From The Dead" allows you to enjoy all the grief and regret felt by your network at your passing, combined with the most dramatic entrance of them all.',
+      price: 500
+    }),
+    Product.create({
+      name: 'Love Triangle',
+      description:
+        "It's a classic for a reason! This is an imbroglio for you and one of your most drama-thirsty single friends.",
+      price: 80
+    }),
+    Product.create({
+      name: 'Evil Stepmother',
+      description:
+        'Feel like Cinderella at the beginning of the story with one of our vindictive and exacting evil stepmoms. She\'s a part of your family now and there\'s nothing you can do about it! Toil away getting the house ready for her weird bookclub friends, partake in the different and bad ways she wants to celebrate holidays, and just generally have your space invaded by "Carol".',
+      price: 240
+    }),
+    Product.create({
+      name: 'Chased by a Mob',
+      description:
+        "Who's that person? Why are they being chased down the street by a huge mob?! Are they an angry mob, or do they just want to bathe in the star power of someone famous? YOU DECIDE! We work with you to craft your perfect mob scenario so you can live it instead of just dreaming about it all the time!",
+      price: 75
+    }),
+    Product.create({
+      name: 'In Love with a Criminal',
+      description:
+        'The Cadillac of dramatic living, nothing says, "I make big, rash decisions that will end in ruin," more than falling in love with someone who is terrible for you! Also you just met them last night and you\'re married now! Oh, did I mention they\'re wanted by the FBI for forging government documents? Laura from work is going to eat this up',
+      price: 1000
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
